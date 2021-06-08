14c4a812d8020e3b6a6b7e8ca7050f442a0ad9df
Renan Bonani <bonani.silva@gmail.com>
2021-06-08 14:16:19 -0300

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { Formik, Field, Form } from 'formik';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default () => {
 
-export default App;
+  function onSubmit(values, actions) {
+    console.log('SUBMIT', values);
+  }
+
+  function onBlurCep(ev, setFieldValue) {
+    const { value } = ev.target;
+
+    const cep = value?.replace(/[^0-9]/g, '');
+
+    if (cep?.length !== 8) {
+      setFieldValue('cidade', 'CEP invalido');
+    }
+
+    fetch(`https://brasilapi.com.br/api/cep/v1/{${cep}}`)
+      .then((res) => res.json())
+      .then((data) => {   
+        if (!data.city) {
+          setFieldValue('cidade', 'CEP invalido');
+        }
+        else{
+          const cidadeAtendida = ['Andradina','Araçatuba','Birigui', 'Guararapes', 'Jales', 'Mirandópolis','Penápolis','Promissão','Três Lagoas'];
+          const verificar = cidadeAtendida.includes(data.city);
+          if( verificar )
+            setFieldValue('cidade', data.city +' Cidade Atendida');
+          else setFieldValue('cidade', data.city);
+        }         
+      });
+    }
+
+
+  return ( 
+    <Formik
+      onSubmit={onSubmit}
+      validateOnMount
+      initialValues={{
+        cep: '',
+        cidade: '',
+      }}
+      
+      render={({ setFieldValue }) => (          
+        <Form className="conteiner">
+          <div className="form-grup">
+            <h1>Busca de Cidades</h1>
+            <h3 className="sub-titulo">Digite o CEP no campo abaixo.</h3>          
+            <Field name="cep" type="text" onBlur={(ev) => onBlurCep(ev, setFieldValue)} className="dados" placeholder="Digite o seu CPF" />
+            <h3>Não sabe o seu CEP ? <a href="https://buscacepinter.correios.com.br/app/endereco/index.php" target="_blank">Pesquise aqui</a></h3>
+            <Field name="cidade" type="text" className="dados cidade" placeholder="Cidade" disabled />
+          </div>         
+        </Form>
+      )}
+    />
+  )}  
+
